dcb748e4c9e3d1315e67eaa18c7ad4c4571e844d
phuong giao <2004phuonggiao@gmail.com>
2023-08-03 03:15:42 +0700

    footer &responsive

--- js/index.js
+++ js/index.js
@@ -8,3 +8,22 @@ function removeLoading() {
 
 // Gọi hàm xóa loading sau 3 giây
 setTimeout(removeLoading, 2000);
+
+//điều chỉnh icon trên menu navbar nhanh
+document.addEventListener('DOMContentLoaded', function () {
+  var menu = document.querySelector('.menu_qc');
+  var pseudo = document.querySelector('.quick_choices');
+  var icon_menu = document.querySelector('.menu_qc i');
+
+  menu.addEventListener('click', function () {
+      if (pseudo.style.display === 'none') {
+          pseudo.style.display = 'block';
+          icon_menu.classList.remove('fa-bars');
+          icon_menu.classList.add('fa-x');
+      } else {
+          pseudo.style.display = 'none';
+          icon_menu.classList.remove('fa-x');
+          icon_menu.classList.add('fa-bars');
+      }
+  });
+});
